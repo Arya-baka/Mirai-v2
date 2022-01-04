6c7edfdb9150054825759d1a496fbe9e2ffc84b4
春野サクラ <92246276+HarunoSakura-2608@users.noreply.github.com>
2022-01-04 11:24:16 +0700

    Update checktt.js

--- modules/commands/checktt.js
+++ modules/commands/checktt.js
@@ -1,85 +1,97 @@
 module.exports.config = {
-	name: "checktt",
-	version: "1.0.5",
-	hasPermssion: 0,
-	credits: "Mirai Team",
-	description: "Kiểm tra lượt tương tác trong nhóm",
-	commandCategory: "system",
-	usages: "[all/tag]",
-	cooldowns: 5,
-    envConfig: {
-        "maxColumn": 10
-    }
+    name: "checktt",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "D-Jukie",
+    description: "Check tương tác các thành viên trong 1 nhóm",
+    commandCategory: "Nhóm",
+    usages: "[tag/reply/all]",
+    cooldowns: 5
 };
-
-module.exports.languages = {
-    "vi": {
-        "all": "%1/ %2 với %3 tin nhắn\n",
-        "mention": "%1 đứng hạng %2 với %3 tin nhắn",
-        "myself": "Bạn đang đứng hạng %1 với %2 tin nhắn"
-    },
-    "en": {
-        "all": "%1/ %2 with %3 messages\n",
-        "mention": "%1 on top %2 with %3 messages",
-        "myself": "You are on top %1 with %2 messages "
+module.exports.onLoad = function() {
+    const { writeFileSync, existsSync } = require('fs-extra');
+    const { resolve } = require("path");
+    const pathA = require('path');
+    const path = pathA.join(__dirname, 'cache', 'checktt.json');
+    if (!existsSync(path)) {
+        const obj = []
+        writeFileSync(path, JSON.stringify(obj, null, 4));
     }
 }
-
-module.exports.run = async function ({ args, api, event, Currencies, getText }) {
-    var mention = Object.keys(event.mentions);
-    try {
-        const data = await api.getThreadInfo(event.threadID);
-        if (args[0] == "all") {
-            var number = 0, storage = [], exp = [];
-            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-            for (const user of storage) {
-                const countMess = await Currencies.getData(user.id);
-                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp});
+module.exports.handleEvent = async({ event, Users }) => {
+    console.log(event.type)
+    const { threadID, senderID, body } = event;
+    const fs = require("fs");
+    const pathA = require('path');
+    const thread = require('./cache/checktt.json');
+    const path = pathA.join(__dirname, 'cache', 'checktt.json');
+    if(event.isGroup == false) return;
+    if (thread.some(i => i.threadID == threadID) == false) {
+        const data = [];
+        for (let user of event.participantIDs) {
+            var name = (await Users.getData(user)).name;
+            var id = user;
+            var exp = 0;
+            if(name !== undefined && name != 'Người dùng Facebook') {
+                data.push({ name, id , exp })
             }
-            exp.sort(function (a, b) { return b.exp - a.exp });
-
-            var page = 1;
-            page = parseInt(args[1]) || 1;
-            page < -1 ? page = 1 : "";
-            var limit = 10;
-            var msg = "Độ tương tác trong box:\n";
-            var numPage = Math.ceil(exp.length/limit);
-
-            for(var i = limit*(page - 1); i < limit*(page-1) + limit; i++){
-                if(i >= exp.length) break;
-                let infoUser = exp[i];
-                msg += `${i+1}.${infoUser.name}: ${infoUser.exp} tin nhắn\n=========\n`
-            }
-
-            msg += `--Trang ${page}/${numPage}--\nDùng ${global.config.PREFIX}checktt all số trang`
-            return api.sendMessage(msg, event.threadID);
         }
-        else if (mention[0]) {
-            var storage = [], exp = [];
-            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-
-            for (const user of storage) {
-                const countMess = await Currencies.getData(user.id);
-                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
-            }
-            exp.sort(function (a, b) { return b.exp - a.exp });
-
-            const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(mention[0])) + 1;
-            const infoUser = exp[rank - 1];
-            return api.sendMessage(getText("mention", infoUser.name, rank, infoUser.exp), event.threadID);
+        thread.push({ threadID, data: data });
+        fs.writeFileSync(path, JSON.stringify(thread, null, 2));
+    }
+    else {
+        var threadData = thread.find(i => i.threadID == threadID && i.threadID !== undefined)
+        if (threadData.data.some(i => i.id == senderID) == false) {
+            var name = (await Users.getData(senderID)).name;
+            var id = senderID;
+            var exp = 0;
+            threadData.data.push({ name, id, exp });
+            fs.writeFileSync(path, JSON.stringify(thread, null, 2));
         }
         else {
-            var storage = [], exp = [];
-            for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
-            for (const user of storage) {
-                const countMess = await Currencies.getData(user.id);
-                exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+            var userData = threadData.data.find(i => i.id == senderID);
+            userData.exp = userData.exp + 1;
+            fs.writeFileSync(path, JSON.stringify(thread, null, 2));
+        }
+    }
+}
+module.exports.run = async function ({ args, api, event }) {
+    const { threadID, senderID, messageID, type, mentions } = event;
+    var mention = Object.keys(mentions);
+    const thread = require('./cache/checktt.json');
+    const data = thread.find(i => i.threadID == threadID)
+    if (args[0] == "all") {
+        var msg = "", exp = [], i = 1, count = 0
+        for(const user of data.data) {
+            exp.push({ name: user.name, exp: user.exp, id: user.id });
+        }
+        exp.sort(function (a, b) { return b.exp - a.exp });
+        for (const user of exp) { 
+            count += user.exp
+            msg += `[${i++}]: ${user.name} với ${user.exp} tin nhắn.\n`
+        }
+        return api.sendMessage(`[====KIỂM TRA TƯƠNG TÁC===]\n\n` + msg + `\nTổng số tin nhắn của nhóm là ${count}`, threadID, messageID);
+    }
+    else 
+        if(type == "message_reply") { mention[0] = event.messageReply.senderID }
+        if (mention[0]) {
+            var exp = [], count = 0
+            for(const user of data.data) {
+                count += user.exp
+                exp.push({ name: user.name, exp: user.exp, id: user.id });
             }
             exp.sort(function (a, b) { return b.exp - a.exp });
-
-            const rank = exp.findIndex(info => parseInt(info.uid) == parseInt(event.senderID)) + 1;
-            const infoUser = exp[rank - 1];
-            return api.sendMessage(getText("myself", rank, infoUser.exp), event.threadID);
+            const rank = exp.findIndex(i => i.id == mention[0])
+            return api.sendMessage(`👤Tên: ${exp[rank].name}\n🏆Rank: ${rank + 1}\n💬Tin nhắn: ${exp[rank].exp}\n💹Tỉ lệ tương tác: ${(exp[rank].exp/count*100).toFixed(0)}%`, threadID, messageID);
         }
-    } catch (e) { return console.log(e) }
+    else {
+        var exp = [], count = 0
+        for(const user of data.data) {
+            count += user.exp
+            exp.push({ name: user.name, exp: user.exp, id: user.id });
+        }
+        exp.sort(function (a, b) { return b.exp - a.exp });
+        const rank = exp.findIndex(i => i.id == senderID);
+        return api.sendMessage(`👤Tên: ${exp[rank].name}\n🏆Rank: ${rank + 1}\n💬Tin nhắn: ${exp[rank].exp}\n💹Tỉ lệ tương tác: ${(exp[rank].exp/count*100).toFixed(0)}%`, threadID, messageID);
+    }
 }
