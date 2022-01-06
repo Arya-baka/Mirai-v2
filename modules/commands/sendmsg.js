9ed3632375bb41fe000e283e50a139481b5b797e
春野サクラ <92246276+HarunoSakura-2608@users.noreply.github.com>
2022-01-06 17:36:40 +0700

    Update sendmsg.js

--- modules/commands/sendmsg.js
+++ modules/commands/sendmsg.js
@@ -10,38 +10,37 @@ module.exports.config = {
 };
 
 	module.exports.run = async ({ api, event, args, getText }) => {
-		if (!args[0]) return api.sendMessage("Bạn chưa nhập nội dung cần gửi",event.threadID,event.messageID);
+		var idbox = args[0];
+		var reason = args.slice(1);
+		if (args.length == 0) api.sendMessage("Syntax error, use: sendmsg ID_BOX [lời nhắn]", event.threadID, event.messageID);
+	else 
+	    if(reason == "")api.sendMessage("Syntax error, use: sendmsg ID_BOX [lời nhắn]", event.threadID, event.messageID);
 		if (event.type == "message_reply") {
-		const request = global.nodemodule["request"];
-		const fs = require('fs')
-		const axios = require('axios')
-		
-		var getURL = await request.get(event.messageReply.attachments[0].url);
-		
-				var pathname = getURL.uri.pathname;
-		
-				var ext = pathname.substring(pathname.lastIndexOf(".") + 1);
-		
-				var path = __dirname + `/cache/snoti`+`.${ext}`;
-		
-		
-		
-		var abc = event.messageReply.attachments[0].url;
-			let getdata = (await axios.get(`${abc}`, { responseType: 'arraybuffer' })).data;
-		
-		  fs.writeFileSync(path, Buffer.from(getdata, 'utf-8'));
-		
-    
-	var idbox = args[0];
-    var reason = args.slice(1);
-
-	if (args.length == 0) api.sendMessage("Syntax error, use: sendmsg ID_BOX [lời nhắn]", event.threadID, event.messageID);
-	
-	else if(reason == "")api.sendMessage("Syntax error, use: sendmsg ID_BOX [lời nhắn]", event.threadID, event.messageID);
-	
-	else
-		api.sendMessage({body:"» Thông báo từ admin tới nhóm bạn «\n\n" + reason.join(" "), attachment: fs.createReadStream(path) }, idbox, () =>
+			const request = global.nodemodule["request"];
+			const fs = require('fs')
+			const axios = require('axios')
+			
+			var getURL = await request.get(event.messageReply.attachments[0].url);
+			
+					var pathname = getURL.uri.pathname;
+			
+					var ext = pathname.substring(pathname.lastIndexOf(".") + 1);
+			
+					var path = __dirname + `/cache/snoti`+`.${ext}`;
+			
+			
+			var abc = event.messageReply.attachments[0].url;
+				let getdata = (await axios.get(`${abc}`, { responseType: 'arraybuffer' })).data;
+			
+			  fs.writeFileSync(path, Buffer.from(getdata, 'utf-8'));	
+	await api.sendMessage({body:"» Thông báo từ admin tới nhóm bạn «\n" + reason.join(" "), attachment: fs.createReadStream(path) }, idbox, () =>
 			api.sendMessage(`${api.getCurrentUserID()}`, () =>
 				api.sendMessage("Đã gửi lời nhắn: " + reason.join(" "), event.threadID)));
-}
+} 
+else {
+		await api.sendMessage("» Thông báo từ Admin tới nhóm bạn «\n" + reason.join(" "), idbox, () =>
+			api.sendMessage(`${api.getCurrentUserID()}`, () =>
+				api.sendMessage("Đã gửi lời nhắn: " + reason.join(" "), event.threadID)));
+
 	}
+}
