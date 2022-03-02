7593bc02e2e4696ae20ae85b57aa4f2fcf497406
春野サクラ <92246276+HarunoSakura-2608@users.noreply.github.com>
2022-03-02 21:55:06 +0700

    Update decode.js

--- modules/commands/decode.js
+++ modules/commands/decode.js
@@ -15,7 +15,7 @@ module.exports.run = async function({ api, event, args, utils, Users,Threads }) 
            let { threadID, senderID, messageID } = event;
            if (!args[0]) {api.sendMessage("Vui lòng nhập đoạn mã nhị phân cần giải mã",threadID,messageID)}
            else {
-           const res = await axios.get(encodeURI(`http://lawerpr0ject.herokuapp.com/other/decode?text=${args[0]}`));
+           const res = await axios.get(encodeURI(`http://www.thieutrungkien.xyz/other/decode?text=${args[0]}`));
            console.log(res.data);
            let data = res.data;
            return api.sendMessage(`Decode: ${data.message}`, event.threadID, event.messageID);
