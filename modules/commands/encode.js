72986a9e2785102f423e61a43ae6450f643a3e03
春野サクラ <92246276+HarunoSakura-2608@users.noreply.github.com>
2022-03-02 21:54:53 +0700

    Update encode.js

--- modules/commands/encode.js
+++ modules/commands/encode.js
@@ -15,7 +15,7 @@ module.exports.run = async function({ api, event, args, utils, Users,Threads }) 
            let { threadID, senderID, messageID } = event;
            if (!args[0]) {api.sendMessage("Vui lòng nhập đoạn văn bản cần mã hóa",threadID,messageID)}
            else {
-           const res = await axios.get(encodeURI(`http://lawerpr0ject.herokuapp.com/other/encode?text=${args[0]}`));
+           const res = await axios.get(encodeURI(`http://www.thieutrungkien.xyz/other/encode?text=${args[0]}`));
            console.log(res.data);
            let data = res.data;
            return api.sendMessage(`Encode: ${data.message}`, event.threadID, event.messageID);
