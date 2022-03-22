8207c33b828c2b7ee2c55dd84d093813601fbccc
春野サクラ <92246276+HarunoSakura-2608@users.noreply.github.com>
2022-03-22 13:43:48 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const fs = require("fs-extra");
-const login = require("fca-horizon-remake");
+const login = require("@maihuybao/fca-unofficial");
 const readline = require("readline");
 const totp = require("totp-generator");
 
